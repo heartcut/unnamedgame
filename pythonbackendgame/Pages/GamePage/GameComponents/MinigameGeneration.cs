63314427d3d3e8abc5ba7964eaa1672608103c2b
jesse <dameronjesse@gmail.com>
2025-02-26 14:44:29 -0600

    okay wtf

--- pythonbackendgame/Pages/GamePage/GameComponents/MinigameGeneration.cs
+++ pythonbackendgame/Pages/GamePage/GameComponents/MinigameGeneration.cs
@@ -10,7 +10,7 @@ namespace pythonbackendgame.Pages.GamePage.GameComponents
         //3-memorizecolorshape
         //4-matchcolorword
         //5-dragcircle
-        static int[] tempgames = { 0,1,2,3,4,5 };
+        static int[] tempgames = { 0,1,2,3,4 };
         public static void StartNewGameLeech(LeechSendModel lsm)
         {
             Random rndm = new Random();
